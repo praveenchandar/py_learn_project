45c63ef89eaefe7cdd6932944b0af25261ff4c44
Praveen Siva <sivasp@uksfwfuji.taec.toshiba.com>
2021-04-08 20:07:03 +0100

    Add Fibonacci & user input handler

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-def print_stuff():
-    s1 = "Hello "
-    s2 = "World\n"
+def strings():
+    print ('strings():-')
+    s1 = "\nHello "
+    s2 = "World"
     s3 = """ 
 what in 
 the world is happening with \
@@ -18,5 +19,48 @@ the world is happening with \
     print (s4[:-18])  # slicing of the string characters, (i.e. without specifying the start or end after ':')
 
 
-# function list
-print_stuff()
+def lists():
+    print ('lists():-')
+    squares = [0]
+    for i in range(1, 10):
+        squares += [i ** 2]  # adding new list elements to expand the list
+    for i in range(11, 15):
+        squares.append(i ** 2)  # adding new list elements to expand the list
+    print (squares)
+    squares1 = squares[2:5]  # slicing returns another list
+    print (squares1)
+
+    # print ('length of list = ', len(list1))
+    # for i in list1:
+    # print ("list(%d)", list1[0])
+
+
+def fibonacci(n):
+    print ('fibonacci(', n, '):-', end='')
+    i, a, b = 0, 0, 1  # multiple assignment
+    while (i < n):
+        print(a, end=',')
+        i, a, b = (i + 1), b, a + b  # multiple assignments, right hand side of '=' is evaluated first (from left to right)
+
+
+def user_prompt():
+    print ('********************************************************************')
+    # function list
+    loop = True
+    while loop:
+        fn = ['exit', 'strings', 'lists', 'fibonacci']
+        print(fn, end=',')
+        user_input = input('\nChoose option:')
+        print('-----------------------')
+        valid = False
+        for entry in fn:
+            if user_input == 'exit':
+                print('--- exit done ---\n')
+                loop = False
+                break
+            elif user_input == entry:
+                eval(entry + '()')
+                print ('\n********************************************************************')
+
+
+user_prompt()
